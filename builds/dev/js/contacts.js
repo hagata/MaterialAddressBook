a926210210d0af7f01afdf30a7acee5f5a847533
genki <genki@hagata.info>
2015-02-16 18:59:27 -0800

    adds angular, adds basic CRUD interaction

---
+++ builds/dev/js/contacts.js
@@ -0,0 +1,29 @@
+{
+    "contacts": [
+        {
+            "id": 1,
+            "name":    "Jenny",
+            "number":  "867-5309",
+            "picture": "http://placehold.it/200x200"
+        },
+             {
+            "id": 2,
+            "name":    "Samantha",
+            "number":  "867-5309",
+            "picture": "http://placehold.it/200x200"
+        },
+             {
+            "id": 3,
+            "name":    "Tbot",
+            "number":  "861-5309",
+            "picture": "http://placehold.it/200x200"
+        },
+             {
+            "id": 4,
+            "name":    "phil",
+            "number":  "361-5094",
+            "picture": "http://placehold.it/200x200"
+        }
+
+    ]
+}
--- components/js/app.js
+++ components/js/app.js
@@ -1,7 +1,74 @@
 
+
 var makeBookApp = angular.module('makeBookApp', ['ngRoute']);
 
-makeBookApp.controller('CardController', ['$scope', function($scope){
-	//Test
-	$scope.name = "angular is up!";
+makeBookApp.controller('CardController', ['$scope','$http', function($scope, $http){
+// ======= Development
+	// $scope.breakout = true;
+	$scope.msg='no message';
+	// $scope.name = "angular is up!";
+// ======= Development
+
+
+// Data
+//	
+
+	$scope.contacts =[];
+
+	$http.get('js/contacts.js').success(function(data){
+		//breakout data
+		$scope.rawData = data;
+		$scope.rawLength = data.contacts.length;
+		$scope.rawObj = data.contacts[0].name;
+		//
+
+		$scope.contacts = data.contacts;
+
+	});
+
+// addContact
+	$scope.addContact = function(){
+
+		$scope.contacts.push({
+			name: $scope.createname,
+			number: $scope.createnumber,
+			picture: "http://placehold.it/200x200"
+			
+		}).then(function(){
+	    		$scope.createname = ''; 
+	    	});
+
+		// $scope.showCreate = !$scope.showCreate;
+	};
+
+// Remove/Delete contact
+	$scope.contactDelete = function(index){
+		$scope.contacts.splice(index,1);
+	};
+
+}]);
+
+makeBookApp.controller('EditController', ['$scope', function($scope){
+
+	$scope.nested="unconfirmed";
+	// Set Default Values
+	$scope.editContactName = $scope.contact.name;
+	$scope.editContactNumber = $scope.contact.number;
+	$scope.editSubmit = function(index){
+		
+
+		$scope.contacts[index] = {
+
+			name: $scope.editContactName,
+			number:$scope.editContactNumber,
+			picture:"http://placehold.it/200x200"
+		};
+
+	};
+
+	$scope.contactEditConfirm = function(){
+		$scope.nested="confirm";
+		// $scope.editShow = false;
+	};
+	
 }]);
